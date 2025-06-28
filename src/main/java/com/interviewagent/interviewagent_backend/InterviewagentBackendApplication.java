bc3ae140a23ffc4ca97318264d8f7872e87c5ae3
Akshara Nair <68681614+akshranair@users.noreply.github.com>
2025-06-28 13:47:28 +0530

    Initial commit for InterviewAgent backend

---
+++ src/main/java/com/interviewagent/interviewagent_backend/InterviewagentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.interviewagent.interviewagent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewagentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewagentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interviewagent/interviewagent_backend/InterviewagentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewagent.interviewagent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewagentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
